e4d0e5c67b496d786d93e78da9e01774132c388c
Takashi <th2826@columbia.edu>
2022-01-04 19:47:33 -0500

    Completed

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState }  from "react";
 
-function PlantCard() {
+function PlantCard( {plant} ) {
+  const { name, image, price} = plant
+
+  const [isInStock, setIsInStock] = useState(true)  
+
+  function handleInOutStockClick(){
+    setIsInStock(!isInStock)
+  }
+
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={image} alt={"plant name"} />
+      <h4>{name}</h4>
+      <p>Price: {price}</p>
+      {isInStock ? (
+        <button className="primary" onClick={handleInOutStockClick}>In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleInOutStockClick}>Out of Stock</button>
       )}
     </li>
   );
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [filterBy, setFilterBy] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((res) => res.json())
+      .then((response) => setPlants(response))
+  }, [])
+
+  function handleAddPlant(newPlant){
+    setPlants([...plants, newPlant])
+  }
+
+  function handleSeach(search){
+    setFilterBy(search)
+  }
+
+  
+  const filteredPlants = plants.filter((plant) => {
+    return plant.name.toLowerCase().includes(filterBy.toLowerCase())
+  })
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant}/>
+      <Search onSearch={handleSeach} filterBy={filterBy}/>
+      <PlantList plants={filteredPlants}/>
     </main>
   );
 }
